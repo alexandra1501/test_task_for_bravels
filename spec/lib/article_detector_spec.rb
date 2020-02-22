f6a58f9002da6f8c7a3b8426d7af77264ef90dff
Alexandra <sgrjaznova@gmail.com>
2020-02-22 16:11:55 +0200

    Fixed spec

--- spec/lib/article_detector_spec.rb
+++ spec/lib/article_detector_spec.rb
@@ -6,9 +6,7 @@ RSpec.describe ArticleDetector do
 
   it('creates article from given html') do
     expect do
-      link = 'https://www.bluehost.com/blog/wordpress/joomla-vs-wordpress-vs-drupal-13427/'
-      html = Nokogiri::HTML(open(link))
-      described_class.sanitize_data(html.css('body'), link, resource.id)
+      described_class.open_by_url(resource.url, resource.id)
     end.to change { Article.count }.by(1)
   end
 end
